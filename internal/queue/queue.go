458372b408944f3dbbf91393eb085e855e546b72
celineleedle <celineleedle@gmail.com>
2026-02-26 15:54:27 -0500

    put stats struct into queue file

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -21,6 +21,14 @@ type TaskQueue struct {
 	closed bool
 }
 
+type Stats struct {
+	NumTasks      int `json:"total_tasks"`
+	NumPending    int `json:"num_pending"`
+	NumProcessing int `json:"num_processing"`
+	NumCompleted  int `json:"num_completed"`
+	NumFailed     int `json:"num_failed"`
+}
+
 func NewTaskQueue() *TaskQueue {
 	lock := &sync.RWMutex{}
 
--- internal/queue/stats.go
+++
@@ -1,9 +0,0 @@
-package queue
-
-type Stats struct {
-	NumTasks      int `json:"total_tasks"`
-	NumPending    int `json:"num_pending"`
-	NumProcessing int `json:"num_processing"`
-	NumCompleted  int `json:"num_completed"`
-	NumFailed     int `json:"num_failed"`
-}
