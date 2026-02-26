844d5d2e1da47ff55bb7c940b9414e9aaf540f26
celineleedle <celineleedle@gmail.com>
2026-02-26 17:35:57 -0500

    update tests with names

--- internal/model/enums_test.go
+++ internal/model/enums_test.go
@@ -7,18 +7,19 @@ import (
 
 func TestPriorityToString(t *testing.T) {
 	tests := []struct {
+		name  string
 		input Priority
 		want  string
 	}{
-		{PriorityHigh, "high"},
-		{PriorityMed, "medium"},
-		{PriorityLow, "low"},
-		{0, fmt.Sprintf("unknown: %d", 0)},
-		{99, fmt.Sprintf("unknown: %d", 99)},
+		{"test_high", PriorityHigh, "high"},
+		{"test_med", PriorityMed, "medium"},
+		{"test_low", PriorityLow, "low"},
+		{"test_unknown_0", 0, fmt.Sprintf("unknown: %d", 0)},
+		{"test_unknown_99", 99, fmt.Sprintf("unknown: %d", 99)},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.want, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			res := tt.input.String()
 			if res != tt.want {
 				t.Errorf("Expected %q, got %q", tt.want, res)
@@ -29,19 +30,20 @@ func TestPriorityToString(t *testing.T) {
 
 func TestParsePriority(t *testing.T) {
 	tests := []struct {
+		name    string
 		input   string
 		want    Priority
 		wantErr bool
 	}{
-		{"high", PriorityHigh, false},
-		{"med", PriorityMed, false},
-		{"medium", PriorityMed, false},
-		{"low", PriorityLow, false},
-		{"bad-input", 0, true},
+		{"test_high", "high", PriorityHigh, false},
+		{"test_med", "med", PriorityMed, false},
+		{"test_medium", "medium", PriorityMed, false},
+		{"test_low", "low", PriorityLow, false},
+		{"test_bad_input", "bad-input", 0, true},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.input, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			res, err := ParsePriority(tt.input)
 			if err != nil && !tt.wantErr {
 				t.Errorf("Got an error but did not expect one: %q", err.Error())
